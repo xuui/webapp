90fd758b589d7ab262ca15f6994c7b21e4d5d7ad
Xu.hel <zvzhssj@gmail.com>
2014-03-08 03:16:55 +0800

    fix

--- resources/appinit.js
+++
@@ -1,8 +0,0 @@
-(function(){(
-function(jQuery){
-	$=jQuery.noConflict();
-	var $window=$(window),$document=$(document);
-	$('article').height(function(index,value){
-		return $window.height()-$('header').height()-$('footer').height();
-	});
-})(jQuery);})();
